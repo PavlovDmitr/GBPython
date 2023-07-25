892116330c7d4b5636bbd7d44fe484615a967d97
PavlovDmitriy <feelerok@gmail.com>
2023-07-25 21:41:19 +0500

    init l1_hw1

---
+++ lesson1/hwtask1.py
@@ -0,0 +1,33 @@
+
+def chekins(ins: str):
+    
+
+    pass
+
+
+def main():
+    print('Check triangle: ')
+    sides = {'a': 0, 'b': 0, 'c':0}
+    keys = sides.keys()
+    for side in keys:
+        print('Please enter a side {}:'.format(side))
+        side_str = input()
+        while (not side_str.isdigit()) or int(side_str) < 0:
+            print('Please enter a valid number greater than zero, {}: '.format(side))
+            side_str = input()
+            
+        sides.update({side: int(side_str)})
+    if  ((sides.get('a') > sides.get('b') + sides.get('c')) or
+            (sides.get('b') > sides.get('a') + sides.get('c')) or
+            (sides.get('c') > sides.get('b') + sides.get('a'))):
+        print('the triangle does not exist')
+        return
+    if  sides.get('a') == sides.get('b') or sides.get('a') == sides.get('c') or sides.get('b') == sides.get('c'):
+        if sides.get('b') != sides.get('c') or sides.get('a') != sides.get('c') or sides.get('b') != sides.get('a'):
+            print('the triangle is isosceles')
+        else: print('the triangle is equilateral')
+        pass
+    print(sides)
+
+if __name__ == "__main__":
+    main()
