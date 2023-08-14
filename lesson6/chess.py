778d34c39e2fba368b9774e47dc291c5ab5ebe64
PavlovDmitriy <feelerok@gmail.com>
2023-08-14 21:36:57 +0500

    edit lesson6

--- lesson6/chess.py
+++ lesson6/chess.py
@@ -15,46 +15,52 @@ def check_8_queens(combination: list) -> bool:
     
     
     for item in combination:
-        for i in range(7):
-            if (i == item[0] and 1 in _table[item[0]]) or _table[item[0]][item[1]] == 2:
+        for i in range(8):
+            if (_table[item[0]][item[1]] == 1 or _table[item[0]][item[1]] == 2): #(i == item[0] and 1 in _table[item[0]]) or _table[item[0]][item[1]] == 2:
+                for item2 in _table:
+                    print(item2)
+                print(i, item[0], item[1])
+                print(_table[item[0]])
+                print(_table[item[0]][item[1]])
                 return False
-            elif i == item[0]: 
-                for j in range(7): 
-                    _table[i][j] = 1
+            
                     
-            else: 
-                _table[i][item[1]] = 1
-                switch = 4
-                x = 1
-                y = 1
-                while switch > 0:
-                    if item[0] + x <= 7 and item[1] + y <= 7: 
-                        _table[item[0] + x][item[1] + y] = 2
-                    if item[0] + x >= 7 or item[1] + y >= 7:
-                        switch -= 1
-                        
-                    if item[0] + x <= 7 and item[1] - y >= 0: 
-                        _table[item[0] + x][item[1] - y] = 2
-                    if item[0] + x >= 7 or item[1] - y <= 0:
-                        switch -= 1
-                        
-                    if item[0] - x >= 0 and item[1] + y <= 7: 
-                        _table[item[0] - x][item[1] + y] = 2
-                    if item[0] - x <= 0 or item[1] + y >= 7:
-                        switch -= 1
-                        
-                    if item[0] - x >= 0 and item[1] - y >= 0: 
-                        _table[item[0] - x][item[1] - y] = 2
-                    if item[0] - x <= 0 or item[1] - y <= 0:
-                        switch -= 1
-                        
-                    x += 1
-                    y += 1
-                    pass
-
+            
+        for k in range(0,8):
+            _table[item[0]][k] = 2
+            _table[k][item[1]] = 2
+        _table[item[0]][item[1]] = 1
+        switch = 4
+        x = 1
+        y = 1
+        while switch > 0:
+            if item[0] + x < 8 and item[1] + y < 8: 
+                _table[item[0] + x][item[1] + y] = 2
+            if item[0] + x >= 8 or item[1] + y >= 8:
+                switch -= 1
+                
+            if item[0] + x <= 7 and item[1] - y >= 0: 
+                _table[item[0] + x][item[1] - y] = 2
+            if item[0] + x >= 7 or item[1] - y <= 0:
+                switch -= 1
+                
+            if item[0] - x >= 0 and item[1] + y <= 7: 
+                _table[item[0] - x][item[1] + y] = 2
+            if item[0] - x <= 0 or item[1] + y >= 7:
+                switch -= 1
+                
+            if item[0] - x >= 0 and item[1] - y >= 0: 
+                _table[item[0] - x][item[1] - y] = 2
+            if item[0] - x <= 0 or item[1] - y <= 0:
+                switch -= 1
+                
+            x += 1
+            y += 1
+            
+    
 
                     
                 
-
+    for item2 in _table:
+        print(item2)
     return True
-
--- lesson6/hwtask1.py
+++ lesson6/hwtask1.py
@@ -1,7 +1,7 @@
 from portfolio import calculate_portfolio_value  as calc_value
 from portfolio import calculate_portfolio_return as calc_retur
 from portfolio import get_most_profitable_stock as most_profit
-from chess import check_8_queens as check8
+from chess import check_8_queens2 as check8
 
 
 STOKS_OLD = {"AAPL": 10, "GOOGL": 5, "MSFT": 8}
@@ -10,8 +10,8 @@ PRICES_OLD = {"AAPL": 150.25, "GOOGL": 2500.75, "MSFT": 300.50}
 STOKS_NEW = {"AAPL": 10, "GOOGL": 5, "MSFT": 8}
 PRICES_NEW = {"AAPL": 155.25, "GOOGL": 2600.75, "MSFT": 800.50}
 _SAFE_COMB_EXAMPLE = [(0, 0), (6, 1), (4, 2), (7, 3),
-(1, 4), (3, 5), (5, 6), (2, 7)]
-_SAFE_COMB_EXAMPLE2 = [(3, 1), (7, 1), (0, 2), (2, 3),
+(1, 4), (3, 5), (6, 6), (2, 7)]
+_SAFE_COMB_EXAMPLE2 = [(3, 0), (7, 1), (0, 2), (2, 3),
 (5, 4), (1, 5), (6, 6), (4, 7)]
 
 def main():
@@ -21,7 +21,7 @@ def main():
     print("Цена портфеля на текущий момент : {}".format(new_price))
     print("Доходность портфеля на текущий момент: {}".format(calc_retur(old_price, new_price)))
     print("Самое прибыльное вложение: {}".format(most_profit(stocks=STOKS_NEW, prices=PRICES_NEW)))
-    print(check8(_SAFE_COMB_EXAMPLE2))
+    print(check8(_SAFE_COMB_EXAMPLE))
 
     pass
 
