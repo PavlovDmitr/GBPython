7dbde599486e24fde7734d4e64af0e33773dc910
PavlovDmitriy <feelerok@gmail.com>
2023-09-28 23:01:26 +0500

    ref les15

--- lesson15/mail_model.py
+++ lesson15/mail_model.py
@@ -1,13 +1,41 @@
+from keywords import keywords
 
 class Mail:
+    '''
+    subj - subject mail,
+    body - body mail,
+    tags - tags mail.
+    '''
+    def __select_tags(self, body:str) -> list:
+        result = []
+        
+        for key in keywords.keys():
+            for tag in keywords.get(key):
+                if len(tag)>3 and body.lower().find(tag) > -1:
+                    result.append(key)
+                    break
+        return result
 
-    def __init__(self,index: int = 0, subj: str = '', body:str = '') -> None:
+        pass
+
+
+    def __init__(self,index: int = 0, body: str = '', subj:str = '') -> None:
         self.index = index
         self.subj = subj
-        self.tags = []
+        self.tags = self.__select_tags(body)
         self.body = body
 
         pass
 
     def set_tag(self, tag):
         self.tags.append(tag)
+
+    def __str__(self) -> str:
+        tags = [f'#{t}' for t in self.tags]
+        return f'{self.index}: {self.subj}\n{self.body}\n'+", ".join(tags)
+        pass
+
+    def __repr__(self) -> str:
+        tags = [f'#{t}' for t in self.tags]
+        return f'{self.index}: {self.subj}\n{self.body}\n'+", ".join(tags)
+        pass
--- lesson15/main.py
+++ lesson15/main.py
@@ -1,27 +1,31 @@
 import csv
 from keywords import keywords
 from mail_model import Mail
+import os
 
 def main(file_path: str):
     parts = {}
-    mails = {}
-    mail = {}
-    index = 1
+    mails = []
+    index = 0
     for k in keywords.keys():
         for item in keywords.get(k):
             parts[item] = k
-    with open(file_path, 'r', encoding='utf-8', newline='') as file:
-        csv_file = csv.reader(file, delimiter=';')
-        for mail_line in csv_file:
-            for mail_item in mail_line:
-                mail[index] = mail_item
-            for key in parts.keys():
-                if mail_item.find(key):
-                    mails[key] = index
-                    index += 1
+    try:
+        with open(file_path, 'r', encoding='utf-8', newline='') as file:
+            csv_file = csv.reader(file, delimiter=';')
+            for mail_line in csv_file:
+                for mail_item in mail_line:
 
-    for k in mail.keys():
-        print(k, mail[k], )
+                    mails.append(Mail(index, body = mail_item, subj=''))
+                    index += 1
+    except FileNotFoundError:
+        print('Файл не найден')
+        mails.append(None)
+    return mails
 
 if __name__ == '__main__':
-    main('user_support_letters.csv')
+    current_file = os.path.realpath(__file__)
+    current_directory = os.path.dirname(current_file)
+    os.chdir(current_directory)
+    res = main('user_support_letter.csv')
+    print(res[len(res)-1])
